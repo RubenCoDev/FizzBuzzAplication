997282ded6e657773ceba422efb2221b6c99057b
Ruben <rubencorraldev@gmail.com>
2024-02-11 22:51:03 +0100

    change_main_route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Apps\Shared\Controller\ApiController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+route::get('/', ApiController::class);
+
+Route::get('/laravel-main', function () {
     return view('welcome');
 });
